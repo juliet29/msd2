00c06b97aafd72ae69bbf20dba2f4b8f4d1b0c58
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2026-01-24 14:48:54 -0800

    handle windows at idf creation time

--- src/msd2/eplus/main.py
+++ src/msd2/eplus/main.py
@@ -1,7 +1,13 @@
+from collections import Counter
 from pathlib import Path
 
 from loguru import logger
-from replan2eplus.ops.subsurfaces.user_interfaces import EdgeGroup, SubsurfaceInputs
+from replan2eplus.ops.subsurfaces.user_interfaces import (
+    EdgeGroup,
+    SubsurfaceInputs,
+    Edge,
+)
+from rich.pretty import pretty_repr
 from msd2.eplus.interfaces import (
     make_details,
     read_edges_to_ezcase_edge_groups,
@@ -10,6 +16,50 @@ from msd2.eplus.interfaces import (
 from replan2eplus.ezcase.ez import EZ
 from replan2eplus.ops.zones.user_interface import Room
 from msd2.config import WEATHER_FILE
+from replan2eplus.ops.subsurfaces.logic.select import get_zones_by_plan_name
+
+
+def handle_windows(case: EZ, windows_edge_group: EdgeGroup):
+    assert windows_edge_group.type_ == "Zone_Direction"
+
+    seen_zones = Counter()
+
+    def create(edge: Edge):
+        zone_a = get_zones_by_plan_name(edge.space_a, case.objects.zones)
+        seen_zones[zone_a.zone_name] += 1
+        # find first surface with an outside boundary condition
+        potential_surfs = list(
+            filter(
+                lambda x: x.boundary_condition == "outdoors"
+                and x.surface_type == "wall",
+                zone_a.surfaces,
+            )
+        )
+        seen_count = seen_zones[zone_a.zone_name] - 1
+
+        if seen_count > len(potential_surfs):
+            raise IndexError("Seen count > num potential surfs!")
+        # get the direction of the first surf
+        drn = list(potential_surfs)[seen_count].direction
+
+        new_edge = Edge(edge.space_a, drn.name)
+        return new_edge
+
+    new_edges = [create(e) for e in windows_edge_group.edges]
+    new_group = EdgeGroup(
+        new_edges, windows_edge_group.detail, windows_edge_group.type_
+    )
+
+    logger.debug(f"Edges to add: {pretty_repr([i.as_tuple for i in new_group.edges])}")
+    return new_group
+
+
+def handle_edge_groups(case: EZ, edge_groups: list[EdgeGroup]):
+    for ix, eg in enumerate(edge_groups):
+        if eg.type_ == "Zone_Direction":
+            edge_groups[ix] = handle_windows(case, eg)
+
+    return edge_groups
 
 
 def generate_idf(
@@ -18,10 +68,19 @@ def generate_idf(
     case = EZ(output_path=out_path, epw_path=WEATHER_FILE)
     case.add_zones(rooms)
 
+    corrected_edge_groups = handle_edge_groups(case, edge_groups)
+
     subsurface_inputs = SubsurfaceInputs(
-        edge_groups, make_details()  # pyright: ignore[reportArgumentType]
+        corrected_edge_groups, make_details()  # pyright: ignore[reportArgumentType]
     )
     case.add_subsurfaces(subsurface_inputs)
+
+    sinfo = [
+        {"name": s.display_name, "idf_name": s.subsurface_name}
+        for s in case.objects.subsurfaces
+    ]
+
+    logger.info(pretty_repr(sinfo))
 
     case.add_constructions()
     case.add_airflow_network()
@@ -34,13 +93,14 @@ def generate_idf(
 def layout_to_idf(edge_path: Path, layout_path: Path, outpath: Path):
     rooms = read_layout_to_ezcase_rooms(layout_path)
     # only doing one type for now
-    edge_group = read_edges_to_ezcase_edge_groups(edge_path).interior_door_edges
-
+    edge_group_holder = read_edges_to_ezcase_edge_groups(edge_path)
+    egs = [edge_group_holder.interior_door_edges, edge_group_holder.window_edges]
     logger.debug(f"Rooms to add: {[i.name for i in rooms]}")
-    logger.debug(f" Edges to add: {[i.as_tuple for i in edge_group.edges]}")
+    for eg in egs:
+        logger.debug(f"Edges to add: {pretty_repr([i.as_tuple for i in eg.edges])}")
 
     # for now, only consider door edges, need different approach for windows..
     # # also need to do airboundaries
 
-    case = generate_idf(rooms, [edge_group], outpath.parent, run=False)
+    case = generate_idf(rooms, egs, outpath.parent, run=False)
     return case
